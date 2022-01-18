bd8954d6a8a13d81d4c6fc4a3a2c4f49ac02a9b4
Budaházi Bence <budahazi.bence@petrik.hu>
2022-01-18 17:52:22 +0100

    Rendezés megvalósítása

--- src/main/java/com/example/dolgozodemo/controllers/MainController.java
+++ src/main/java/com/example/dolgozodemo/controllers/MainController.java
@@ -16,10 +16,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Comparator;
 
 public class MainController extends Controller {
     @FXML private ListView<Dolgozo> dolgozoList;
     private DolgozoDB db;
+    private int szempont;
+    private int irany;
     @FXML
     private RadioButton radioKor;
     @FXML
@@ -36,6 +39,8 @@ public class MainController extends Controller {
     private RadioButton radioNovekvo;
 
     public void initialize() {
+        szempont = 0;
+        irany = 0;
         try {
             db = new DolgozoDB();
             listaFeltolt();
@@ -95,28 +100,65 @@ public class MainController extends Controller {
         try {
             dolgozoList.getItems().clear();
             dolgozoList.getItems().addAll(db.getDolgozok());
+            listaRendez();
         } catch (SQLException e) {
             Platform.runLater(() -> hibaKiir(e));
         }
     }
 
+    private void listaRendez() {
+        switch (szempont){
+            case 0:
+                if (irany == 0){
+                    dolgozoList.getItems().sort(Comparator.comparing(Dolgozo::getNev));
+                } else {
+                    dolgozoList.getItems().sort(Comparator.comparing(Dolgozo::getNev).reversed());
+                }
+                break;
+            case 1:
+                if (irany == 0){
+                    dolgozoList.getItems().sort(Comparator.comparing(Dolgozo::getEletkor));
+                } else {
+                    dolgozoList.getItems().sort(Comparator.comparing(Dolgozo::getEletkor).reversed());
+                }
+                break;
+            case 2:
+                if (irany == 0){
+                    dolgozoList.getItems().sort(Comparator.comparing(Dolgozo::getFizetes));
+                } else {
+                    dolgozoList.getItems().sort(Comparator.comparing(Dolgozo::getFizetes).reversed());
+                }
+                break;
+        }
+    }
+
     @FXML
-    public void radioKorCllick(ActionEvent actionEvent) {
+    public void radioKorClick(ActionEvent actionEvent) {
+        szempont = 1;
+        listaRendez();
     }
 
     @FXML
     public void radioCsokkenoClick(ActionEvent actionEvent) {
+        irany = 1;
+        listaRendez();
     }
 
     @FXML
-    public void radioFizetesCllick(ActionEvent actionEvent) {
+    public void radioFizetesClick(ActionEvent actionEvent) {
+        szempont = 2;
+        listaRendez();
     }
 
     @FXML
     public void radioNovekvoClick(ActionEvent actionEvent) {
+        irany = 0;
+        listaRendez();
     }
 
     @FXML
-    public void radioNevCllick(ActionEvent actionEvent) {
+    public void radioNevClick(ActionEvent actionEvent) {
+        szempont = 0;
+        listaRendez();
     }
 }
